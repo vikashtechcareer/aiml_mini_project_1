8667664b7b868a98eac15b9f52ae39f37a23e67d
vikash ranjan <techvikashinfo@gmail.com>
2025-04-10 20:56:15 +0530

    initial ab testing

--- titanic_ab_testing/model_v1/evaluate.py
+++ titanic_ab_testing/model_v1/evaluate.py
@@ -2,7 +2,7 @@ import pandas as pd
 import joblib
 from sklearn.metrics import accuracy_score
 
-df = pd.read_csv("../data/Titanic.csv")
+df = pd.read_csv("./data/Titanic.csv")
 df = df[['Pclass', 'Sex', 'Age', 'Survived']].dropna()
 df['Sex'] = df['Sex'].map({'male': 0, 'female': 1})
 
