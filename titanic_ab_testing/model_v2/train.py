c63b3f92417e15cbcca2d50091ce9397687c9316
vikash ranjan <techvikashinfo@gmail.com>
2025-04-10 20:40:26 +0530

    initial ab testing

--- titanic_ab_testing/model_v2/train.py
+++ titanic_ab_testing/model_v2/train.py
@@ -1,7 +1,7 @@
 import joblib
 from sklearn.ensemble import RandomForestClassifier
 
-from aiml_mini_project_1.aiml_mini_1.titanic_ab_testing.model_v1.train import X_train, y_train
+from aiml_mini_project_1.titanic_ab_testing.model_v1.train import X_train, y_train
 
 # same preprocessing as model_v1
 
