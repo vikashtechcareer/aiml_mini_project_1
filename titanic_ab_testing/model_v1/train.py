01d16091c357d75ad82f4f9a5f0db1fab244f321
vikash ranjan <techvikashinfo@gmail.com>
2025-04-10 20:54:03 +0530

    initial ab testing

--- titanic_ab_testing/model_v1/train.py
+++ titanic_ab_testing/model_v1/train.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import joblib
 
-df = pd.read_csv('../data/train.csv')
+df = pd.read_csv('../data/Titanic.csv')
 df = df[['Pclass', 'Sex', 'Age', 'Survived']].dropna()
 df['Sex'] = df['Sex'].map({'male': 0, 'female': 1})
 
