142d49f47453c6c94f8a8367ffa117193425b0e7
vikash ranjan <techvikashinfo@gmail.com>
2025-04-10 21:08:29 +0530

    initial ab testing

--- titanic_ab_testing/model_v2/train.py
+++ titanic_ab_testing/model_v2/train.py
@@ -1,11 +1,21 @@
 import joblib
 from sklearn.ensemble import RandomForestClassifier
 
-from aiml_mini_project_1.titanic_ab_testing.model_v1.train import X_train, y_train
 
-# same preprocessing as model_v1
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import joblib
 
-# Just change the model
+print( os.getcwd() )
+df = pd.read_csv('titanic_ab_testing/data/Titanic.csv')
+df = df[['Pclass', 'Sex', 'Age', 'Survived']].dropna()
+df['Sex'] = df['Sex'].map({'male': 0, 'female': 1})
+
+X = df[['Pclass', 'Sex', 'Age']]
+y = df['Survived']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
