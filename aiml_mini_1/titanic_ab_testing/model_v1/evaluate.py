87ad1316b86a05ac3aa4d3f2335a6c642894e774
vikash ranjan <techvikashinfo@gmail.com>
2025-04-10 20:19:56 +0530

    initial ab testing

---
+++ aiml_mini_1/titanic_ab_testing/model_v1/evaluate.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import joblib
+from sklearn.metrics import accuracy_score
+
+df = pd.read_csv('../data/train.csv')
+df = df[['Pclass', 'Sex', 'Age', 'Survived']].dropna()
+df['Sex'] = df['Sex'].map({'male': 0, 'female': 1})
+
+X = df[['Pclass', 'Sex', 'Age']]
+y = df['Survived']
+
+model = joblib.load('model_v1.pkl')
+y_pred = model.predict(X)
+
+print(f"Accuracy (Model V1): {accuracy_score(y, y_pred)}")
---
+++ aiml_mini_1/titanic_ab_testing/model_v1/train.py
@@ -0,0 +1,18 @@
+# Logistic Regression model
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import joblib
+
+df = pd.read_csv('../data/train.csv')
+df = df[['Pclass', 'Sex', 'Age', 'Survived']].dropna()
+df['Sex'] = df['Sex'].map({'male': 0, 'female': 1})
+
+X = df[['Pclass', 'Sex', 'Age']]
+y = df['Survived']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+joblib.dump(model, 'model_v1.pkl')
